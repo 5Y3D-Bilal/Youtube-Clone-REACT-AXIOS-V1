1e957928437e73de3b87a7110c9129d77e0340d7
】【爪尺 Ꮆ卂爪乇 尺卂几Ꮆ乇尺 <54587dfdd@gmail.com>
2023-10-11 15:08:05 +0500

    first commit

--- src/Pages/homepage.jsx
+++ src/Pages/homepage.jsx
@@ -2,6 +2,7 @@ import Nav from "../Component/nav";
 import Sidebar from "../Component/sidebar";
 import Toptrending from "../Component/toptrending";
 import Application from "../Component/mainyoutubepage";
+import './../App.css'
 
 function Homepage() {
   return (
